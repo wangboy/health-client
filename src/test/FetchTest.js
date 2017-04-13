4a720a84faebd0f0fed924c7d1b151bfb1982505
wangboy <bjutwangboy@gmail.com>
2017-04-13 18:45:59 +0800

    add fetch init obj

--- src/test/FetchTest.js
+++ src/test/FetchTest.js
@@ -12,7 +12,11 @@ class FetchTest extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {url: "www.baidu.com"};
+    this.state = {
+      url: "int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=218.4.255.12",
+      fetchResult: "waiting",
+      fetchData: ""
+    };
 
     this.handleClick = this.handleClick.bind(this);
     this.handleInput = this.handleInput.bind(this);
@@ -26,12 +30,35 @@ class FetchTest extends Component {
   handleClick(e) {
     e.preventDefault();
     console.log("fetch url : " + this.state.url)
-    fetch(this.state.url)
+    this.setState({fetchResult: "waiting"});
+
+    var init = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default',
+        headers: {
+          // 'Access-Control-Allow-Origin': "*",
+          // 'Accept': 'application/x-www-form-urlencoded',
+          // 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
+          // 'Authorization':'Bearer e3a4ca4b-bdaa-4034-a842-3641055819a3'
+        }
+      }
+    ;
+
+
+    fetch(this.state.url, init)
       .then((response) => {
-        console.log("fetch result :" + response.status)
-      }).catch((e) => {
-      console.log(" fetch error ")
-    })
+        console.log("fetch result :" + response.status + "_" + response.ok)
+        this.setState({fetchResult: "success " + response.status + "_" + response.ok});
+        return response.text()
+      })
+      .then(data => {
+        this.setState({fetchData: data})
+      })
+      .catch((e) => {
+        console.log(" fetch error ")
+        this.setState({fetchResult: "error" + e})
+      })
   }
 
   render() {
@@ -39,6 +66,8 @@ class FetchTest extends Component {
       <div>
         <input type="text" onChange={this.handleInput} value={this.state.url}/>
         <button onClick={this.handleClick}>Fetch</button>
+        <p>Fetch result : {this.state.fetchResult}</p>
+        <p>Fetch Data: {this.state.fetchData}</p>
       </div>
     )
   }
