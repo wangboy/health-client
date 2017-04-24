b2f492b7053e771ea8ccac22800f3fd70de86abb
wangboy <bjutwangboy@gmail.com>
2017-04-24 18:32:23 +0800

    add react@1.5.2, add admin test

--- src/health/Posts.js
+++ src/health/Posts.js
@@ -1,17 +1,59 @@
-/**
- * Created by wangbo on 22/04/2017.
- */import React from 'react';
-import { List, Datagrid, TextField, ReferenceField } from 'admin-on-rest/lib/mui';
+import React from "react";
+import {
+  Create,
+  Datagrid,
+  DisabledInput,
+  Edit,
+  EditButton,
+  List,
+  LongTextInput,
+  ReferenceField,
+  ReferenceInput,
+  SelectInput,
+  SimpleForm,
+  TextField,
+  TextInput
+} from "admin-on-rest/lib/mui";
 
 export const PostList = (props) => (
   <List {...props}>
     <Datagrid>
-      <TextField source="id" />
+      <TextField source="id"/>
       <ReferenceField label="User" source="userId" reference="users">
-        <TextField source="name" />
+        <TextField source="name"/>
       </ReferenceField>
-      <TextField source="title" />
-      <TextField source="body" />
+      <TextField source="title"/>
+      <TextField source="body"/>
+      <EditButton />
     </Datagrid>
   </List>
+);
+
+const PostTitle = ({record}) => {
+  return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = (props) => (
+  <Edit title={<PostTitle />} {...props}>
+    <SimpleForm>
+      <DisabledInput source="id"/>
+      <ReferenceInput label="User" source="userId" reference="users">
+        <SelectInput optionText="name"/>
+      </ReferenceInput>
+      <TextInput source="title"/>
+      <LongTextInput source="body"/>
+    </SimpleForm>
+  </Edit>
+);
+
+export const PostCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+        <SelectInput optionText="name"/>
+      </ReferenceInput>
+      <TextInput source="title"/>
+      <LongTextInput source="body"/>
+    </SimpleForm>
+  </Create>
 );
